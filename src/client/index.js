e15d18bb71d3430e7c1b1ffb0d3e93d656e22e13
Nanthini <nanthinin92@gmail.com>
2020-04-11 22:30:23 -0500

    feat: Basic JS functionality for form items.
    1. Added event listener for Submit button.
    2. Validation of user inputs.

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,15 @@
-alert("Testing..")
+import { eventListeners } from './js/initializeAndHandleListeners'
+import { destinationValidator } from './js/destinationValidator'
+import { dateValidator } from './js/dateValidator'
 
 import './styles/styles.scss'
 import './styles/header_footer.scss'
 import './styles/content.scss'
+
+window.addEventListener('DOMContentLoaded', eventListeners)
+
+export {
+    destinationValidator,
+    dateValidator
+}
 
